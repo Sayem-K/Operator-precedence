b104c7829079fa914efc4dbada5ea56516b65ce7
hoigoysohio <njuli@yahoo.com>
2024-12-19 18:25:46 +0000

    done

---
+++ main.py
@@ -0,0 +1,15 @@
+a = 5
+b = 8
+c = 2
+d = 1
+e = 3
+f = -2
+
+w = (e + c) / a + (b - d)
+x = (c - e) * -2 - (a - b)
+y = c * 2 - (d - a) * f
+z = (3 * b) / (c * 2 * 2 * 2)  
+print(w)
+print(x)
+print(y)
+print(z)
